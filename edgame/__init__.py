a5f33c457807d1d8eec49f23961f8a2258695789
Dylan Wilson <dylanjw@protonmail.com>
2020-08-15 23:38:40 -0700

    edgars work

---
+++ edgame/__init__.py
@@ -0,0 +1,17 @@
+#definitions
+
+
+personsname = input("type in your name - ")
+place = input("where are you - ")
+def function_printpersonsnameandplace(thename, theplace):
+    print(
+            "the persons name is "
+            + thename
+            + " and they are in the place called "
+            + theplace
+    )
+
+
+#executable shit
+
+function_printpersonsnameandplace(personsname, place)
